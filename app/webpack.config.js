e31178fe068a5e81cdb4c81dbff1fdd928af5f15
Miklos Danka <danka.miklos@gmail.com>
2018-08-25 21:54:29 +0200

    webpack now specifies an absolute publicpath

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -18,10 +18,12 @@ module.exports = {
     },
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),},
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: "/",
+    },
     resolve: {
         // Add `.ts` and `.tsx` as a resolvable extension.
-        extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js']
+        extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js'],
     },
     module: {
         preLoaders: [
