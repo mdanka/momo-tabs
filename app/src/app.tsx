818bd605be0fcd90339e8473a3260631df1be328
Miklos Danka <danka.miklos@gmail.com>
2018-08-25 14:05:12 +0200

    Added the store provider

--- app/src/app.tsx
+++ app/src/app.tsx
@@ -1,10 +1,18 @@
 import "es6-shim";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import { GuitarApp } from "./components";
+import { createAppStore } from "./store";
 
 const appElement = document.getElementById("app");
+const store = createAppStore();
 
 if (appElement != null) {
-    ReactDOM.render(<GuitarApp />, appElement);
+    ReactDOM.render(
+        <Provider store={store}>
+            <GuitarApp />
+        </Provider>,
+        appElement,
+    );
 }
