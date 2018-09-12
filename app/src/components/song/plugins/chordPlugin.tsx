209127b0f7f2fa13606a65d4baf5ad4a1aacb4e3
Miklos Danka <danka.miklos@gmail.com>
2018-09-12 23:35:58 +0200

    Added support for 5 chords

--- app/src/components/song/plugins/chordPlugin.tsx
+++ app/src/components/song/plugins/chordPlugin.tsx
@@ -30,7 +30,7 @@ export function ChordPlugin(options?: IChordPluginOptions): Plugin {
     const NOTES = ["Ab", "A", "A#", "Bb", "B", "C", "C#", "Db", "D", "D#", "Eb", "E", "F", "F#", "Gb", "G", "G#"];
     const CHORDS: string[] = [];
     NOTES.forEach(note => {
-        CHORDS.push(note, note + "7", note + "m", note + "m7", note + "maj7");
+        CHORDS.push(note, `${note}5`, `${note}7`, `${note}m`, `${note}m7`, `${note}maj7`);
     });
 
     const regexRangeToSlateRange = (key: string) => (regexRange: RegexUtils.IRegexMatchRange): RangeJSON => {
