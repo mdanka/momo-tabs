ba486fbb8017604be75efa664bbeaf804d75688b
Miklos Danka <danka.miklos@gmail.com>
2018-11-03 14:41:20 +0100

    The client app now removed the server-side material-ui styling

--- src/clientApp.tsx
+++ src/clientApp.tsx
@@ -1,20 +1,39 @@
 import "es6-shim";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { createAppStore } from "./store";
+import { createAppStore, IAppState } from "./store";
 import { App } from "./app";
 import { BrowserRouter } from "react-router-dom";
 import { initializeAndGetClientSideServices } from "./services";
+import { Store } from "redoodle";
+
+interface IClientAppProps {
+    store: Store<IAppState>;
+}
+
+export class ClientApp extends React.Component<IClientAppProps, {}> {
+    public componentDidMount() {
+        // Remove the server-side injected CSS
+        const jssStyles = document.getElementById("material-ui-jss-server-side");
+        if (jssStyles && jssStyles.parentNode) {
+            jssStyles.parentNode.removeChild(jssStyles);
+        }
+    }
+
+    public render() {
+        const { store } = this.props;
+        return (
+            <BrowserRouter>
+                <App store={store} />
+            </BrowserRouter>
+        );
+    }
+}
 
 const appElement = document.getElementById("app");
 const store = createAppStore();
 initializeAndGetClientSideServices(store);
 
 if (appElement != null) {
-    ReactDOM.render(
-        <BrowserRouter>
-            <App store={store} />
-        </BrowserRouter>,
-        appElement,
-    );
+    ReactDOM.render(<ClientApp store={store} />, appElement);
 }
